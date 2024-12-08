86d7db1ad4bf9b04ba5acda51094f46218a2ee64
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2024-12-08 15:44:28 -0300

    `Refactor album controller to use a single export and add methods for album CRUD operations`

--- src/controller/album.controller.js
+++ src/controller/album.controller.js
@@ -1,19 +1,48 @@
 import { albumService } from "../services/album.service.js";
 
-const getAlbums = async (req, res) => {
-    try {
-        const albums = await albumService.getAllAlbums()
-        res.json({albums})
-    } catch (error) {
-        console.log(error);
-        if (error instanceof Error) {
-            res.status(500).json({ message: error.message })
-            return
-        }
-        res.status(500).json({ error: "Error de Servidor" })
-    }
-}
-
 export const albumController = {
-    getAlbums
-}
+    async getAllAlbums(req, res, next) {
+        try {
+            const albums = await albumService.getAllAlbums();
+            res.status(200).json(albums);
+        } catch (error) {
+            next(error); // Delegar el error al middleware de manejo de errores
+        }
+    },
+
+    async getAlbumById(req, res, next) {
+        try {
+            const album = await albumService.getAlbumById(req.params.id);
+            res.status(200).json(album);
+        } catch (error) {
+            next(error); // Delegar error al middleware
+        }
+    },
+
+    async createAlbum(req, res, next) {
+        try {
+            const album = await albumService.createAlbum(req.body);
+            res.status(201).json({ album });
+        } catch (error) {
+            next(error); // Delegar error al middleware
+        }
+    },
+
+    async updateAlbum(req, res, next) {
+        try {
+            const album = await albumService.updateAlbum(req.params.id, req.body);
+            res.status(200).json(album);
+        } catch (error) {
+            next(error); // Delegar error al middleware
+        }
+    },
+
+    async deleteAlbum(req, res, next) {
+        try {
+            await albumService.deleteAlbum(req.params.id);
+            res.status(204).send(); // 204: No Content
+        } catch (error) {
+            next(error); // Delegar error al middleware
+        }
+    },
+};
